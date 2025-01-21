caee95f0d49263c6b95e6c0e67cad10ce1ead034
FojleTuhin <fojletuhin@gmail.com>
2025-01-21 23:15:36 +0600

    git init

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bebas_Neue } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const bebasNeue = Bebas_Neue({
+  variable: "--font-bebas",
   subsets: ["latin"],
+  weight: '400',   // 400, 700, 900
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={bebasNeue.className}
       >
         {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,34 @@
-import Image from "next/image";
+import React from "react";
+import { HiArrowRight } from "react-icons/hi2";
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <div className="bg-[url('https://s3-alpha-sig.figma.com/img/b187/d92c/3b7e4dc21dbe8aa3a95c2041b952805a?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lMnFNXkWU-qRTAB7QS7kSXIivTXoslDmDYaaAXxdyVivse3P9SeENEEshTwnBQOO2WvU0gmgfAMpNKDqPU5sNLIudkqOBUs6ycVAwoqkjfpNJfLgClUrgWHw0m1xaQKH2a7pbBEGOipTZKj8jR7HtqoRJcEHzez38WVMJUYc5-ZbNctfeOoxdBBGpkoP9rLSDj8svAm5Nji96IhTQZ9ZQz0F5dwF92YvnhBvg0985D3VMHwZPS76GY3DaTlT5DyC7lZcbZpNDGd1AfTS5iFuoIwwnviosiZIYn9LWHWfAqAWd9LgD~NrCA8HGn6wS5cXt-ECF9EbWlmNEY7Ygx5CsA__')] bg-cover bg-center h-screen  px-32 py-20">
+        <p className="text-[#fff]">
+          SOS Children&apos;s Villages Bangladesh and BESTSELLER Foundation.
+        </p>
+        <p className="text-[52px] text-[#fff]">
+          Inspiring Journeys of Youth <br /> Entrepreneurs
+        </p>
+        <p className="text-[18px] mt-4 mb-8">
+          {" "}
+          Entrepreneurship and Basic Skills Training of 200 Youths in Bogura and{" "}
+          <br /> Chattogram to Ensure Sustainable Sources of Income.
+        </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex gap-10">
+          <button className="bg-none text-[#fff] border border-white  px-6 py-3 ">
+            Learn More
+          </button>
+          <button className="flex items-center gap-2 text-white  ">
+            Explore Projects
+            <HiArrowRight className="text-xl" />
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
-}
+};
+
+export default page;
